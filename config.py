483969d5248a3bdfcf8aed7d467791ed382e01c7
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-11-05 10:18:49 +0000

    Ops changes

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 
 
 class Config(object):
@@ -6,14 +7,16 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    DATABASE_NAME = 'working'
-    DATABASE_USER = 'landcharges'
-    DATABASE_PASSWORD = 'lcalpha'
-    DATABASE_HOST = 'localhost'
+    DATABASE_NAME = os.getenv('DATABASE_NAME', 'working')
+    DATABASE_USER = os.getenv('DATABASE_USER', 'lc-working-data')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'lcalpha')
+    DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME)
 
 
 class PreviewConfig(Config):
-    DATABASE_NAME = 'working'
-    DATABASE_USER = 'landcharges'
-    DATABASE_PASSWORD = 'lcalpha'
-    DATABASE_HOST = 'localhost'
+    DATABASE_NAME = os.getenv('DATABASE_NAME', 'working')
+    DATABASE_USER = os.getenv('DATABASE_USER', 'lc-working-data')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'lcalpha')
+    DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME)
