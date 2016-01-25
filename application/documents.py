a4dc849d3ed27958f443015cb319121fee65bc2a
Chris O'Riordan <chris.oriordan@digital.landregistry.gov.uk>
2016-01-25 09:40:39 +0000

    covert tiff to jpg

--- application/documents.py
+++ application/documents.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import io
 
 
 def complete(cursor):
@@ -22,12 +24,24 @@ def get_document(cursor, document_id):
 def get_image(cursor, document_id, page_number):
     cursor.execute("select content_type, image from documents where document_id=%(doc_id)s and page=%(page)s",
                    {"doc_id": document_id, "page": page_number})
-
     rows = cursor.fetchall()
-
     if len(rows) == 0:
         return None
-
     row = rows[0]
     # Python returns blob as memoryview object, convert back to bytes
-    return {"bytes": bytes(row['image']), "mimetype": row['content_type']}
+    contenttype = row['content_type']
+    b = bytes()
+    if contenttype == 'image/tiff':
+        try:
+            # if this is a tiff it needs to be converted to jpg for display
+            img_bytes = io.BytesIO()
+            im = Image.open(io.BytesIO(bytes(row['image'])))
+            im.save(img_bytes, format='JPEG')
+            x = img_bytes.getbuffer()
+            b = bytes(x)
+            contenttype = 'image/jpeg'
+        except Exception as exp:
+            print("could not covert tiff to jpeg: " + str(exp))
+    else:
+        b = bytes(row['image'])
+    return {"bytes": b, "mimetype": contenttype}
